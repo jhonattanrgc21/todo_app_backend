d4cc88c5b82603abb32f1a60221da520cad950fa
Jhonattan Garcia <jgarciac@intelix.biz>
2022-08-01 14:11:31 -0400

    Agregue la entidad Task

---
+++ src/app/entities/tasks.entity.ts
@@ -0,0 +1,39 @@
+import { Column, Entity } from 'typeorm';
+import UuidEntity from "./uuid.entity";
+
+@Entity('tasks')
+export default class Task extends UuidEntity{
+
+	// Atributes
+	@Column({
+		type: 'text',
+		nullable: false,
+		comment: "Descripcion de la tarea"
+	})
+	private name: string;
+
+	@Column({
+		type: 'bool',
+		comment: "¿Finalizada?",
+		default: 0
+	})
+	private check: boolean;
+
+	// Getters
+	public getName(): string{
+		return this.name;
+	}
+
+	public getCheck(): boolean{
+		return this.check;
+	}
+
+	// Setters
+	public setName(newName: string){
+		this.name = newName;
+	}
+
+	public setCheck(newCheck: boolean){
+		this.check = newCheck;
+	}
+}
